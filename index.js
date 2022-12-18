b567118aadf0bba8254d55a39e62d9003519dc60
Pierce <pierceevans@me.com>
2022-12-18 12:52:51 -0600

    commit tree f028c6b57b7a3c940ba1089cb4bbbce990d3d3d1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+    var customerName = 'bob';
+    function upperCaseCustomerName(){
+        customerName = 'BOB';
+       return(customerName);
+    }
+    function setBestCustomer(){
+        bestCustomer = 'not bob';
+        return(bestCustomer);
+    }
+    function overwriteBestCustomer(){
+        bestCustomer = 'maybe bob';
+    }
+    const leastFavoriteCustomer = 'jack';
+    function changeLeastFavoriteCustomer(){
+        leastFavoriteCustomer = 'steve';
+    }
